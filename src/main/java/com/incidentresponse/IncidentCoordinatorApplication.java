6d0f9dde043bee577df21f50a07b65c917ec941c
jpak <48459170+jpak0@users.noreply.github.com>
2025-10-14 22:53:25 -0700

    Initial project setup with Maven, and Spring Boot 3.5.6

---
+++ src/main/java/com/incidentresponse/IncidentCoordinatorApplication.java
@@ -0,0 +1,13 @@
+package com.incidentresponse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IncidentCoordinatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IncidentCoordinatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/incidentresponse/IncidentCoordinatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.incidentresponse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IncidentCoordinatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
